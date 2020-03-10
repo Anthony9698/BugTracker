b5e0412747f89ab4239aac09188a7765b99b74a7
Anthony <anthonyviera4@gmail.com>
2020-03-10 17:27:01 -0500

    Added a static files directory for modifying bootstrap css.

--- src/bugtracker/urls.py
+++ src/bugtracker/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from profiles import views as profile_views
 
 urlpatterns = [
@@ -24,3 +26,7 @@ urlpatterns = [
     path('Projects/', profile_views.projects, name='projects'),
     path('Tickets/Dashboard', profile_views.dashboard, name='dashboard'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
