7421e66ef8602787c17659d6578a75cfa2999f05
Anthony Viera <anthonyviera4@gmail.com>
2020-05-30 04:59:48 -0500

    Switched database from sqlite to postgresql and added ticket and project models.

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(default="")
+    project_id = models.BigIntegerField(),
+    priority = models.CharField(max_length=50)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    date_added = models.DateField()
