23dd227ae6fd1c40f2b2a49a8db52406af30bc2b
Anthony Viera <anthonyviera4@gmail.com>
2020-05-31 17:09:48 -0500

    Updated models to include a user field.

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project, Ticket
+from .models import Project, Ticket, User
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Ticket)
+admin.site.register(User)
 
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=255, null=False, unique=True)
+    join_date = models.DateTimeField(default=now, editable=False)
+    last_login = models.DateTimeField(default=now, editable=False)
+    password = models.CharField(max_length=64, null=False)
+
+
 class Project(models.Model):
-    title = models.CharField(max_length=100, null=False)
-    date_added = models.DateField()
+    title = models.CharField(max_length=64, null=False)
+    description = models.TextField()
+    date_added = models.DateField(default=now, editable=False)
+    users = models.ManyToManyField(User)
+
 
 class Ticket(models.Model):
-    title = models.CharField(max_length=100, null=False)
+    title = models.CharField(max_length=64, null=False)
     description = models.TextField(default="")
     project_id = models.ForeignKey(Project, default=None, on_delete=models.PROTECT)
-    priority = models.CharField(max_length=50, null=False)
+    priority = models.CharField(max_length=64, null=False)
+
