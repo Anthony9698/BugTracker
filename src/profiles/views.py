164d58827a992f528b58e4c1b66df8cb714be8b2
Anthony Viera <anthonyviera4@gmail.com>
2020-06-05 23:33:58 -0500

    Users can now login.

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,12 +4,36 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from profiles.forms import RegistrationForm
+from profiles.forms import RegistrationForm, LoginForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def login_page(request):
-    return render(request, 'login.html')
+    context = {}
+    user = request.user
+
+    # # user is already logged in, redirect back to dashboard
+    # if user.is_authenticated:
+    #     return redirect("dashboard")
+
+    # user is trying to log in
+    if request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("dashboard")
+
+    else:
+        form = LoginForm()
+
+    context['login_form'] = form
+    return render(request, 'login.html', context)
 
 
 def logout_user(request):
