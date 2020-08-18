dfc2fd54733947ea7339858c3d2604a0a7bfb992
Anthony Viera <anthonyviera4@gmail.com>
2020-08-17 21:54:15 -0500

    Made dashboard page less wide.

--- src/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('overview/', views.api_overview, name="api_overview"),
-]
--- src/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-
-def api_overview(request):
-    return JsonResponse("API BASE POINT", safe=False)
