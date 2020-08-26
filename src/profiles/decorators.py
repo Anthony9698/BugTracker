e6ea174b40ba0fe69643c865f17e122c954c8654
Anthony Viera <anthonyviera4@gmail.com>
2020-08-26 01:13:11 -0500

    Restricted certain endpoints for only admin and project manager roles.

--- src/profiles/decorators.py
+++ src/profiles/decorators.py
@@ -1,11 +1,22 @@
 from functools import wraps
 from django.core.exceptions import PermissionDenied
 
+
 def is_admin(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         user_roles = [role for role in request.user.roles]
         if 'Admin' in user_roles:
+            return function(request, *args, **kwargs)
+        raise PermissionDenied
+    return wrap
+
+
+def is_admin_or_manager(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_roles = [role for role in request.user.roles]
+        if 'Admin' in user_roles or 'Project Manager' in user_roles:
             return function(request, *args, **kwargs)
         raise PermissionDenied
     return wrap
