0cdf6aeb5f0b122f94b6d68cf2887f12ee15f9dd
Anthony Viera <anthonyviera4@gmail.com>
2020-08-28 01:27:12 -0500

    Restricted more views and html elements from being viewed depending on user role.

--- src/profiles/decorators.py
+++ src/profiles/decorators.py
@@ -5,11 +5,30 @@ from django.http import *
 from profiles.utils import get_user_tickets
 
 
+def user_has_role(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_roles = [role for role in request.user.roles]
+        if len(user_roles) > 0:
+            return function(request, *args, **kwargs)
+        raise PermissionDenied
+    return wrap
+
 def is_admin(function):
     @wraps(function)
     def wrap(request, *args, **kwargs):
         user_roles = [role for role in request.user.roles]
         if 'Admin' in user_roles:
+            return function(request, *args, **kwargs)
+        raise PermissionDenied
+    return wrap
+
+
+def is_project_manager(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        user_roles = [role for role in request.user.roles]
+        if 'Project Manager' in user_roles:
             return function(request, *args, **kwargs)
         raise PermissionDenied
     return wrap
@@ -39,4 +58,35 @@ def ticket_exists_viewable(function):
             raise PermissionDenied
         return function(request, pk, *args, **kwargs)
     return wrap
+
+
+def project_exists_viewable(function):
+    @wraps(function)
+    def wrap(request, pk, *args, **kwargs):
+        try:
+            project = Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            raise Http404
+
+        if request.user not in project.users.all():
+            raise PermissionDenied
+        return function(request, pk, *args, **kwargs)
+    return wrap
+
+
+def comment_exists_editable(function):
+    @wraps(function)
+    def wrap(request, pk, *args, **kwargs):
+        user_roles = [role for role in request.user.roles]
+        try:
+            comment = Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+        if request.user != comment.user and'Admin' not in user_roles and 'Project Manager' not in user_roles:
+            raise PermissionDenied
+
+        return function(request, pk, *args, **kwargs)
+    return wrap
+
             
