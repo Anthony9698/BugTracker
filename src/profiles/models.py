ff7ed93f4db4ce00653dd948de1d8ab7639726df
Anthony Viera <anthonyviera4@gmail.com>
2020-08-31 20:16:05 -0500

    Deleted username field from userprofile model.

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,36 +5,6 @@ from multiselectfield import MultiSelectField
 from django.utils.timezone import get_current_timezone
 from datetime import datetime
 
-# defining what I want to happen when a new user/superuser is created
-# class MyProfileManager(BaseUserManager):
-#     # these fields are required when creating a new user/superuser
-#     def create_user(self, email, username, password=None):
-#         if not email:
-#             raise ValueError("Users must have an email address")
-#         if not username:
-#             raise ValueError("Users must have a username")
-
-#         user = self.model(
-#             email = self.normalize_email(email),
-#             username=username,
-#         )
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-
-#     def create_superuser(self, email, username, password):
-#         user = self.create_user(
-#             email = self.normalize_email(email),
-#             password = password,
-#             username=username,
-#         )
-#         user.is_admin = True
-#         user.is_staff = True
-#         user.is_superuser = True
-#         user.is_admin = True
-#         user.save(using=self._db)
-
 
 class MyProfileManager(BaseUserManager):
     # these fields are required when creating a new user/superuser
@@ -70,7 +40,6 @@ class UserProfile(AbstractBaseUser):
     first_name = models.CharField(max_length=64, null=False, default="First Name")
     last_name = models.CharField(max_length=64, null=False, default="Last Name")
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    #username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_admin = models.BooleanField(default=False)
@@ -80,7 +49,6 @@ class UserProfile(AbstractBaseUser):
     roles = MultiSelectField(choices=choices, default='Submitter')
 
     USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = ['username']
 
     objects = MyProfileManager()
 
