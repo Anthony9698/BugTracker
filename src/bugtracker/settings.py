db9f3bcf2b397ef224fecafc7197c78e0a4a9554
Anthony Viera <anthonyviera4@gmail.com>
2020-09-01 13:59:45 -0500

    Users can now reset their passwords if they are forgotten.

--- src/bugtracker/settings.py
+++ src/bugtracker/settings.py
@@ -149,3 +149,12 @@ if DEBUG:
 
 LOGIN_URL = reverse_lazy('login')
 
+
+# SMPT Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
