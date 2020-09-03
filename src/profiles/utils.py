49f45159321459706fc6b1b12cd47193b656b330
Anthony Viera <anthonyviera4@gmail.com>
2020-09-02 20:46:33 -0500

    Developers are not notified through email when they're ticket is reassigned.

--- src/profiles/utils.py
+++ src/profiles/utils.py
@@ -1,3 +1,5 @@
+from django.core import mail
+import os
 from profiles.models import Project, Ticket
 
 
@@ -18,3 +20,30 @@ def get_user_tickets(request, user_roles):
         user_tickets = Ticket.objects.all()
 
     return user_tickets.order_by('-last_modified_date')
+
+
+def send_ticket_assignment_email(user, ticket):
+    with mail.get_connection() as connection:
+                mail.EmailMessage(
+                    "New Ticket Assignment",
+                    "Hello, this message is to inform you that your project manager " + str(user)
+                        + " has assigned you the following ticket: " + str(ticket.title)
+                        + "\n\nThank you for using our site!" + "\n\nThe Bug Tracker team.",
+                    os.environ.get('EMAIL_HOST'),
+                    [ticket.assigned_user.email],
+                    connection=connection,
+                ).send()
+
+
+def send_ticket_reassignment_email(old_user, new_user, ticket):
+    with mail.get_connection() as connection:
+                mail.EmailMessage(
+                    "Ticket Reassignment",
+                    "Hello, this message is to inform you that your project manager " + str(old_user)
+                        + " has reassigned your ticket: " + str(ticket.title) + " to " + str(new_user)
+                        + "\n\nThank you for using our site!" + "\n\nThe Bug Tracker team.",
+                    os.environ.get('EMAIL_HOST'),
+                    [ticket.assigned_user.email],
+                    connection=connection,
+                ).send()
+    send_ticket_assignment_email(new_user, ticket)
