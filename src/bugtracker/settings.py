37363f8851a5b8cafb69699264bc81351a6dac75
Anthony Viera <anthonyviera4@gmail.com>
2020-09-07 06:01:22 -0500

    Turned off debug for production.

--- src/bugtracker/settings.py
+++ src/bugtracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
@@ -138,14 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# if DEBUG:
-#     MEDIA_URL = '/media/'
-#     STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
-#     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
-#     STATICFILES_DIRS = (
-#         os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
-#     )
 
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
