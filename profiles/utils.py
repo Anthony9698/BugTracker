fd6e2ec23be1f7916ec3ddd6a7ae31015eec8061
Anthony Viera <anthonyviera4@gmail.com>
2020-09-12 02:41:19 -0500

    Updated sending ticket rassignment email.

--- profiles/utils.py
+++ profiles/utils.py
@@ -42,16 +42,18 @@ def send_ticket_assignment_email(proj_manager, ticket, host_name):
 
 
 def send_ticket_reassignment_email(proj_manager, old_user, ticket, host_name):
-    with mail.get_connection() as connection:
-        mail.EmailMessage(
-            "Ticket Reassignment",
-            "Hello, this message is to inform you that your project manager " + str(proj_manager) + ","
-                + " has reassigned your ticket: " + str(ticket.title) + " to " + str(ticket.assigned_user) + "."
-                + "\n\nThank you for using our site!" + "\n\nThe Bug Tracker team.",
-            os.environ.get('EMAIL_HOST'),
-            [old_user.email],
-            connection=connection,
-        ).send()
+    subject, from_email, to = 'Ticket Reassignment', os.environ.get('EMAIL_HOST'), old_user.email
+    text_content = "Hello, this message is to inform you that your project manager " + str(proj_manager) + "," \
+                    + " has reassigned your ticket: " + str(ticket.title) + " to " + str(ticket.assigned_user) + "." \
+                    + "\n\nThank you for using our site!" + "\n\nThe Bug Tracker team."
+                    
+    html_content = "<p>Hello, this message is to inform you that your project manager " + str(proj_manager) + "," \
+                    + " has reassigned your ticket: <a href=\"" + str(host_name) + "/tickets/detail/" \
+                    + str(ticket.id) + "/" + "\">" + str(ticket.title) + " to " + str(ticket.assigned_user) + "." \
+                    + "<br>Thank you for using our site!" + "<br>The Bug Tracker team.</p>"
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
     send_ticket_assignment_email(ticket.assigned_user, ticket, host_name)
 
 
